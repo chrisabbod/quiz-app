be1bf62771a23171dc7fad431f04881206bed9f2
Chris <chrisabbod@gmail.com>
2018-04-20 23:51:38 -0400

    <feat>: Make getScore Function and Wire Submit Button
    Make getScore function and wire submit button in MainActivity.java

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void getScore(View v){
+        Toast.makeText(this, "TEST", Toast.LENGTH_LONG).show();
     }
 }
