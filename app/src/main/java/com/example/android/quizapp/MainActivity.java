4feba10ad9df3af3fa6413baf6c82d0e8b1b9402
Chris <chrisabbod@gmail.com>
2018-04-21 01:05:28 -0400

    <feat>: Add Question Four Function
    Add questionFour function that extracts user from EditText field

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.android.quizapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -24,6 +26,9 @@ public class MainActivity extends AppCompatActivity {
     public void getScore(View v){
         int userScore = 0;
         userScore += questionOne();
+        userScore += questionTwo();
+        userScore += questionThree();
+        userScore += questionFour();
 
         Toast.makeText(this, "Your score is " + userScore + " points!", Toast.LENGTH_LONG).show();
     }
@@ -62,6 +67,21 @@ public class MainActivity extends AppCompatActivity {
         RadioButton everestRadioButton = (RadioButton)findViewById(R.id.everest_radio_button);
 
         if(everestRadioButton.isChecked()){
+            return 1;
+        }
+        return 0;
+    }
+
+    /**
+     *
+     * @return 0 or 1 depending on if the user chose the correct answer or not
+     */
+    public int questionFour(){
+        EditText q4EditText = (EditText)findViewById(R.id.q4_edit_text);
+        String answer = q4EditText.getText().toString().toLowerCase();    //Extracts string and sets to lower case to make sure user gets credit for answer if there are upper case letters
+        //Log.d("Question Four", "Answer: " + answer);
+
+        if(answer.equals("huron") || answer.equals("ontario") || answer.equals("michigan") || answer.equals("erie") || answer.equals("superior")){
             return 1;
         }
         return 0;
