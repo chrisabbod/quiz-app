43b541950c5491fde4813a5c9cf18ffad802cd37
Chris <chrisabbod@gmail.com>
2018-04-21 00:01:27 -0400

    <feat>: Make Question One Function
    Make questionOne function in MainActivity.java

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -3,17 +3,34 @@ package com.example.android.quizapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public int userScore = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void getScore(View v){
-        Toast.makeText(this, "TEST", Toast.LENGTH_LONG).show();
+        userScore += questionOne();
+
+        Toast.makeText(this, "Your score is " + userScore + " points!", Toast.LENGTH_LONG).show();
+    }
+
+    public int questionOne(){
+        RadioButton trueButton = (RadioButton)findViewById(R.id.q1_true_radio_button);
+
+        if(trueButton.isChecked()){
+            //Toast.makeText(this, "TRUE", Toast.LENGTH_LONG).show();
+            return 1;
+        }
+
+        return 0;
     }
 }
